126844ea62b304baf3ae9e18e71ae56096aa4993
Miguel Pedraza <mpedraza@lean-tech.io>
2021-03-12 15:25:58 -0500

    Primera clase

---
+++ clases-mauro/clases-mauro/src/ejercicio1.java
@@ -0,0 +1,19 @@
+//Tipos de dato primitivos
+public class ejercicio1 {
+	public static void main(String[] args) {
+		//Numericos (enteros)
+		byte enteroCortisimo;
+		short enteroCorto;
+		int enteroNormal;
+		long enteroLargo; 
+		//Numericos (reales)
+		float realFlotanteNormal = 2.0F;
+		double realPreciso = 2.56784;
+		//cadena
+		char unCaracter = 'A';
+		char[] vectorCaracter = {'M','a','u','r','o'};
+		String cadenaCaracteres = "Mauro";
+		//logicos
+		boolean logico = true;		
+	}
+}
